8bf0c47c97b28178d7a033fe7f4accb095d34c09
saasbook <saasbook@saasbook.(none)>
2014-12-11 21:24:59 -0800

    adapt object_tracking for ros/hydro v2

---
+++ object_tracking/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/ubuntu/ros_workspace/object_tracking/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/ubuntu/ros_workspace/object_tracking/build/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ubuntu/ros_workspace/object_tracking/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/ubuntu/ros_workspace/object_tracking/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/ubuntu/ros_workspace/object_tracking/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/home/ubuntu/ros_workspace/object_tracking/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/ubuntu/ros_workspace/object_tracking/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/ubuntu/ros_workspace/object_tracking/build"
+export PYTHONPATH="/home/ubuntu/ros_workspace/object_tracking/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/ubuntu/ros_workspace/object_tracking/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/ubuntu/ros_workspace/object_tracking/src:/opt/ros/hydro/share:/opt/ros/hydro/stacks"
---
+++ object_tracking/build/object_tracking/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/ros_workspace/object_tracking/src/object_tracking
+# Build directory: /home/ubuntu/ros_workspace/object_tracking/build/object_tracking
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
